b30c6029623c4c42b1c5427f09d81a9b07ac426c
jiandi1234567890 <550392461@qq,com>
2016-08-30 11:06:03 +0800

    text

---
+++ SignupAllinone/TEXTViewController.m
@@ -0,0 +1,37 @@
+//
+//  TEXTViewController.m
+//  SignupAllinone
+//
+//  Created by 张海禄 on 16/8/30.
+//  Copyright © 2016年 广为网络. All rights reserved.
+//
+
+#import "TEXTViewController.h"
+
+@interface TEXTViewController ()
+
+@end
+
+@implementation TEXTViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
